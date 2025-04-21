e8e977159f04fd8a4a9d14d4093508e9cdc3f5b8
Tristan Damron <tristandamron@protonmail.com>
2025-04-20 20:10:48 -0700

    Implement createStore

--- index.ts
+++ index.ts
@@ -1,13 +1,33 @@
 import { test as base } from '@playwright/test';
 
-class Store {
-    static state: Record<string, any> = {};
-}
+/**
+ * A type for a reducer callback function.
+ */
+export type ReducerCallback = (action?: any) => any;
 
 /**
- * A type alias for a Rewright stateful datatype.
+ * A type for a stateful getter function.
  */
-export type State = Record<string, any>;
+export type StateGetter = () => any;
+
+/**
+ * A type for a stateful setter function.
+ */
+export type StateSetter = (newValue: any) => void;
+
+/**
+ * A type for an effect callback function.
+ */
+export type EffectCallback = () => void;
+
+/**
+ * A type for a Rewright stateful object.
+ */
+export type State = { name: string, value: any };
+
+class GlobalStore {
+    static state: Record<string, State> = {};
+}
 
 /**
  * A Playwright test fixture that includes a Rewright stateful object.
@@ -21,16 +41,16 @@ export type State = Record<string, any>;
  *
  * test("Search for query", async ({ page, state }) => {
  *      const googlePOM = new GooglePOM(page);
- *      state.searchQuery = "Cute cat memes";
+ *      state.searchQuery = {name: "searchQuery", value: "Cute cat memes" };
  *      await page.goto("https://google.com");
- *      await googlePOM.searchFor(state.searchQuery);
- *      await expect(googlePOM.searchBox).toContainText(state.searchQuery);
+ *      await googlePOM.searchFor(state.searchQuery.value);
+ *      await expect(googlePOM.searchBox).toContainText(state.searchQuery.value);
  * });
  * ```
  */
-export const test = base.extend<{ state: State }>({
+export const test = base.extend<{ state: Record<string, State> }>({
   state: async ({}, use) => { // eslint-disable-line no-empty-pattern
-    const state = Store.state;
+    const state = GlobalStore.state;
     await use(state);
   },
 });
@@ -49,18 +69,18 @@ export const test = base.extend<{ state: State }>({
  * }
  * ```
  */
-export function useState(name: string, defaultValue?: any): [Function, Function] {
-    if (defaultValue)
-        Store.state[name] = defaultValue;
+export function useState(name: string, defaultValue?: any): [StateGetter, StateSetter] {
+    if (defaultValue !== undefined)
+        GlobalStore.state[name] = { name, value: defaultValue };
 
-    return [() => { return Store.state[name] }, (newValue: any) => {
-        Store.state[name] = newValue;
+    return [() => { return GlobalStore.state[name]!.value }, (newValue: any) => {
+        GlobalStore.state[name] = { name, value: newValue };
     }];
 }
 
 /**
  * A function that triggers a callback event if a state dependency changes.
- * @param {Function} callback - the callback event to trigger.
+ * @param {EffectCallback} callback - the callback event to trigger.
  * @param {string[]} dependencies - the names of the state values to watch.
  *
  * @example
@@ -80,24 +100,23 @@ export function useState(name: string, defaultValue?: any): [Function, Function]
  * }
  * ```
  */
-export function useEffect(callback: Function, dependencies: string[]): any {
+export function useEffect(callback: EffectCallback, dependencies: string[]): void {
     if (dependencies.length === 0)
         callback();
 
     const stateDependencies = dependencies.map((name) => {
-        return {name, "value": Store.state[name]};
+        return {name, "value": GlobalStore.state[name]!.value};
     });
 
     setInterval((prevDependencies) => {
         prevDependencies.forEach((state) => {
-            if (state.value !== Store.state[state.name]) {
+            if (state.value !== GlobalStore.state[state.name]!.value) {
                 callback();
-                state.value = Store.state[state.name];
+                state.value = GlobalStore.state[state.name]!.value;
             }
         })
     }, 0.0000001, stateDependencies);
 }
-
 
 /**
  * A function for managing state, similar to useState.
@@ -106,7 +125,7 @@ export function useEffect(callback: Function, dependencies: string[]): any {
  * pass in values which influnce your reducer's update logic.
  * The managed state is globally available.
  * @param {string} name - the name of the state to be managed.
- * @param {Function} callback - the callback function to trigger when setting the value of the state.
+ * @param {ReducerCallback} callback - the callback function to trigger when setting the value of the state.
  * @param {any} defaultValue - optional; the default value of the state.
  * @returns A getter and setter function for the managed state.
  *
@@ -121,12 +140,44 @@ export function useEffect(callback: Function, dependencies: string[]): any {
  * }
  * ```
  */
-export function useReducer(name: string, callback: Function, defaultValue?: any): [Function, Function] {
+export function useReducer(name: string, callback: ReducerCallback, defaultValue?: any): [StateGetter, ReducerCallback] {
     if (defaultValue)
-        Store.state[name] = defaultValue;
+        GlobalStore.state[name] = { name, value: defaultValue };
 
-    return [() => { return Store.state[name] }, (action: any) => {
-        Store.state[name] = callback(action);
+    return [() => { return GlobalStore.state[name]!.value }, (action: any) => {
+        GlobalStore.state[name] = {
+            name,
+            value: callback(action),
+        };
     }];
+}
+
+/**
+ * A function for creating a state store.
+ * State stores are appended to Rewright's global state store. These objects are helpful for defining
+ * multiple stateful objects in Rewright that are accessible across multiple classes.
+ *
+ * @param {State[]} definition - the stateful objects to append to the global state store.
+ *
+ * @example
+ * ```ts
+ * createStore([
+ *     {
+ *         name: "foo",
+ *         value: true,
+ *     },
+ *     {
+ *         name: "bar",
+ *         value: false,
+ *     }
+ *]);
+ * ```
+ */
+export function createStore(
+    definition: State[]
+) {
+    for (const state of definition) {
+        useState(state.name, state.value);
+    }
 }
 
