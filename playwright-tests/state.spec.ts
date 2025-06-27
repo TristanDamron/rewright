c8e3ad0ea18073e9c9fcb66035f3ebac8d335e7e
Tristan Damron <tristandamron@protonmail.com>
2025-06-26 21:39:23 -0700

    Implement state class decorator

--- playwright-tests/state.spec.ts
+++ playwright-tests/state.spec.ts
@@ -1,7 +1,32 @@
-import { test } from "../index.ts";
-import { expect } from "@playwright/test";
+// @ts-nocheck
 
-test.describe("state fixture", () => {
+import { test, state, type StateSetter, type StateGetter } from "../index.ts";
+import { expect, type Page } from "@playwright/test";
+import { useState } from "../index.ts";
+
+@state("test", true, (page: Page) => {
+    return {
+        "truthy": true,
+        "locator": page.locator("test"),
+        "testTruthy": () => { return "Hello world!" }
+    }
+})
+class SomePOM {
+    readonly getTest: StateGetter;
+    readonly setTest: StateSetter;
+    readonly truthy: boolean;
+
+    constructor(protected page: Page) {
+        this.truthy = false;
+        [this.getTest, this.setTest] = useState("test");
+    }
+
+    public getTruthy(): boolean {
+        return this.truthy;
+    }
+}
+
+test.describe("state fixtures in a playwright test", () => {
     test("can set state value", ({ state }) => {
         state.test = { name: "test", value: true };
         expect(state.test.value).toBe(true);
@@ -9,8 +34,28 @@ test.describe("state fixture", () => {
 
     test("can update state value", ({ state }) => {
         state.test = { name: "test", value: true };
-        state.test = { name: "test", value: false };
+        state.test.value = false;
         expect(state.test.value).toBe(false);
     });
-});
 
+    test("can get state value from fixture from inside of a POM", ({ state, page }) => {
+        state.test = { name: "test", value: true };
+        const somePom = new SomePOM(page);
+        expect(somePom.getTest()).toBe(true);
+    });
+
+    test("can override a POM if a state value is set", ({ state, page }) => { 
+        state.test = { name: "test", value: true };
+        const somePom = new SomePOM(page);
+        expect(somePom.getTruthy()).toBe(true);
+        expect(somePom.testTruthy()).toBe("Hello world!"); 
+        expect(somePom.locator).not.toBe(undefined);
+    });
+
+    test("obfuscates overrides if state is not an expected value", ({ state, page }) => { 
+        state.test = { name: "test", value: false };
+        const somePom = new SomePOM(page);
+        expect(somePom.getTruthy()).toBe(false);
+        expect(somePom.testTruthy).toBe(undefined);
+    });
+});
